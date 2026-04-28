2a6863fd93a712fb845d931722faa705e2493f94
cesar <cesarurielperez63@gmail.com>
2026-04-27 20:44:42 -0600

    se integra main.py con llamadas a funciones

---
+++ main.py
@@ -0,0 +1,10 @@
+import funciones
+
+# Probando las funciones
+resultado_suma = funciones.suma(10, 5)
+resultado_resta = funciones.resta(20, 8)
+resultado_mult = funciones.multiplicacion(4, 3)
+
+print("El resultado de la suma es:", resultado_suma)
+print("El resultado de la resta es:", resultado_resta)
+print("El resultado de la multiplicación es:", resultado_mult)
